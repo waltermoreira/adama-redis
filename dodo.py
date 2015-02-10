0967829ed21192ddf603ffb79489dbf58882ca1f
Walter Moreira <walter@waltermoreira.net>
2015-02-09 20:24:15 -0600

    Rename image to adama/redis

--- dodo.py
+++ dodo.py
@@ -10,7 +10,7 @@ DOIT_CONFIG = {'default_tasks': ['build']}
 def task__check_images():
     """Check dependencies in the form of images"""
 
-    img = 'serfnode'
+    img = 'adama/serfnode'
     return {
         'actions': ["docker inspect -f '{{{{ .Id }}}}' {} "
                     "2>/dev/null".format(img)]
@@ -48,19 +48,19 @@ def remote_image_exists(img):
 def task_build():
     """Build serf-redis image"""
 
-    all_files = ['dodo.py', 'Dockerfile', 'deploy.yml']
+    all_files = ['dodo.py', 'Dockerfile']
     for d, _, fs in os.walk('handler'):
         for f in fs:
             all_files.append(os.path.join(d, f))
 
     return {
-        'actions': ['docker build -t serf-redis .',
-                    'docker inspect -f "{{ .Id }}" serf-redis > .build'],
+        'actions': ['docker build -t adama/redis .',
+                    'docker inspect -f "{{ .Id }}" adama/redis > .build'],
         'targets': ['.build'],
         'file_dep': all_files,
         'task_dep': ['_check_images'],
         'uptodate': [result_dep('_check_images'),
-                     target_image_exists('serf-redis')],
+                     target_image_exists('adama/redis')],
         'clean': True,
         'verbosity': 2
     }
@@ -70,12 +70,11 @@ def task_push():
     """Push image to docker hub"""
 
     return {
-        'actions': ['docker tag -f serf-redis adama/serf-redis',
-                    'docker push adama/serf-redis',
-                    'docker inspect -f "{{ .Id }}" serf-redis > .push'],
+        'actions': ['docker push adama/redis',
+                    'docker inspect -f "{{ .Id }}" adama/redis > .push'],
         'targets': ['.push'],
         'file_dep': ['.build'],
         'task_dep': ['build'],
-        'uptodate': [remote_image_exists('adama/serf-redis')],
+        'uptodate': [remote_image_exists('adama/redis')],
         'verbosity': 2
     }
